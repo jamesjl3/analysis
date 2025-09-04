2b858edd1274738cfd8994247d3657b658260257
jamesjl3 <jennifer.l.james@vanderbilt.edu>
2025-09-03 21:14:48 -0400

    Adding Fun4All to match JetMatchingSubjets.cc/.h

--- JS-Jet/JetValidation/macro/MatchingSubjets/Fun4All_JetMatchingSubjets.C
+++ JS-Jet/JetValidation/macro/MatchingSubjets/Fun4All_JetMatchingSubjets.C
@@ -44,15 +44,16 @@ std::cout << "ROOT Dynamic Path: " << gSystem->GetDynamicPath() << std::endl;
 //gSystem->mkdir("/sphenix/tg/tg01/jets/jamesj3j3/JetMatchingSubjets/", true);
 
 void Fun4All_JetMatchingSubjets(const char *filelisttruth = "dst_truth_jet.list",
-                           const char *filelistcalo = "dst_calo_cluster.list",
-                           const char *filelistglobal = "dst_global.list",
-                           const char *filelisttruthg4hit = "g4hits.list",
-                           const char *outname = "outputest_JetMatchingSubjets.root")
+				const char *filelistcalo = "dst_calo_cluster.list",
+				const char *filelistglobal = "dst_global.list",
+				const char *filelisttruthg4hit = "g4hits.list",
+				const char *outname = "outputest_JetMatchingSubjets.root",
+				int start_event = 0, int end_event = 1000)
 {
 
   std::string jetNode = "AntiKt_Tower_r04_Sub1";
   Fun4AllServer *se = Fun4AllServer::instance();
-  int verbosity = 1;
+  int verbosity = 0;
   
   se->Verbosity(verbosity);
   recoConsts *rc = recoConsts::instance();
@@ -63,15 +64,6 @@ void Fun4All_JetMatchingSubjets(const char *filelisttruth = "dst_truth_jet.list"
   se->registerSubsystem( cent );
   Enable::VERBOSITY = verbosity;
   
-  JetMatchingSubjets *myJetUnf = new JetMatchingSubjets("AntiKt_Tower_r04_Sub1_Calib", "AntiKt_Truth_r04", outname);  
-
-  myJetUnf->setEtaRange(-0.7, 0.7);
-  myJetUnf->setRecoPtMin(5.0);
-  myJetUnf->setTruthPtMin(10.0);
-  myJetUnf->setMatchDRMax(0.2);
-  myJetUnf->setPtBinning({5,10,15,20,25,30,35,40,45,50,55});
-
-  myJetUnf->setZsjBinning({0.00,0.05,0.10,0.15,0.20,0.25,0.30,0.35,0.40,0.45,0.50});
   HIJetReco();
   
   recoConsts::instance()->set_StringFlag("CDB_GLOBALTAG", "MDC2");
@@ -85,30 +77,47 @@ void Fun4All_JetMatchingSubjets(const char *filelisttruth = "dst_truth_jet.list"
   jetcalib->set_ApplyZvrtxDependentCalib(true);
   jetcalib->Verbosity(0);
 
-  se->registerSubsystem(jetcalib);
-   
- //  myJetUnf->doUnsub(1);
+  
+
+  //  myJetUnf->doUnsub(1);
   //  myJetUnf->doSeeds(0);
   //   myJetUnf->set_doSim(false);
   //  myJetUnf->doClusters(0);
+  JetMatchingSubjets *myJetUnf = new JetMatchingSubjets("AntiKt_Tower_r04_Sub1_Calib", "AntiKt_Truth_r04", outname);  
+  myJetUnf->setRecoConstituentJetNode("AntiKt_Tower_r04_Sub1");
+  myJetUnf->setZWindow(-30.f, 30.f);
+  myJetUnf->setEtaRange(-0.7, 0.7);
+  myJetUnf->setRecoPtMin(5.0);
+  myJetUnf->setTruthPtMin(10.0);
+  myJetUnf->setMatchDRMax(0.2);
+  myJetUnf->setPtBinning({5,10,15,20,25,30,35,40,45,50,55});
+  myJetUnf->setZsjBinning({0.00,0.05,0.10,0.15,0.20,0.25,0.30,0.35,0.40,0.45,0.50});
+
+  se->registerSubsystem(jetcalib);
   se->registerSubsystem(myJetUnf);
 
   Fun4AllInputManager *intrue = new Fun4AllDstInputManager("DSTtruth");
-  intrue->AddListFile(filelisttruth);
+   intrue->AddListFile(filelisttruth); //for standalone Fun4All running
+  //  intrue->AddFile(filelisttruth); //for batch running to condor
   se->registerInputManager(intrue);
 
   Fun4AllInputManager *in4 = new Fun4AllDstInputManager("DSTg4hit");
-  in4->AddListFile(filelisttruthg4hit);
+  in4->AddListFile(filelisttruthg4hit); //for standalone Fun4All running
+  //in4->AddFile(filelisttruthg4hit); //for batch running to condor
   se->registerInputManager(in4);
 
   Fun4AllInputManager *in2 = new Fun4AllDstInputManager("DSTcalo");
-  in2->AddListFile(filelistcalo);
+  in2->AddListFile(filelistcalo); //for standalone Fun4All running
+  //in2->AddFile(filelistcalo); //for batch running to condor
   se->registerInputManager(in2);
 
   Fun4AllInputManager *in3 = new Fun4AllDstInputManager("DSTglobal");
-  in3->AddListFile(filelistglobal);
+  in3->AddListFile(filelistglobal); //for standalone Fun4All running
+  //in3->AddFile(filelistglobal); //for batch running to condor
   se->registerInputManager(in3);
-  se->run(3);
+  //  se->run(1000);
+  se->skip(start_event); // Skip events before start_event
+  se->run(end_event - start_event);
   se->End();
   //  gSystem->Exit(0);
   return;
